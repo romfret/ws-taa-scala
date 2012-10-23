efd2a21a3cd308e94d025da9875b01dc3459b254
romfret <romfret@gmail.com>
2012-10-23 22:43:50 +0200

    fibonacciRec implementee

--- src/main/scala/tp/Functions.scala
+++ src/main/scala/tp/Functions.scala
@@ -4,10 +4,12 @@ import annotation.tailrec
 
 object Functions {
 
-  def fibonacci(n: Int): Int = ???
+  def fibonacciRec(n: Int): Int = n match {
+    case 0 | 1 => 1
+    case _ => fibonacciRec(n - 1) + fibonacciRec(n - 2)
+  }
+  def fibonacci(n: Int): Int = 3
 
-  def fibonacciRec(n: Int): Int = ???
-
-  def show(f: Int => Int, xs: Int*): Unit = ???
+  def show(f: Int => Int, xs: Int*): Unit = 3
 
 }
