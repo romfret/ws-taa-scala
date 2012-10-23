edc2bec08e60f0d8daae0da2d120ddbf1b0460b0
romfret <romfret@gmail.com>
2012-10-23 22:24:20 +0200

    premier commit
    le premier commit !

---
+++ src/main/scala/tp/Functions.scala
@@ -0,0 +1,13 @@
+package tp
+
+import annotation.tailrec
+
+object Functions {
+
+  def fibonacci(n: Int): Int = ???
+
+  def fibonacciRec(n: Int): Int = ???
+
+  def show(f: Int => Int, xs: Int*): Unit = ???
+
+}
---
+++ src/main/scala/tp/IntList.scala
@@ -0,0 +1,94 @@
+package tp
+
+/**
+ * IntList public interface
+ */
+sealed abstract class IntList {
+
+  def foreach(f: Int => Unit): Unit
+
+  def map(f: Int => Int): IntList
+
+  def filter(p: Int => Boolean): IntList
+
+  def sum: Int
+
+  def product: Int
+
+  def fold(z: Int, op: (Int, Int) => Int): Int
+
+  def forall(p: Int => Boolean): Boolean
+
+  def foldBool(z: Boolean, op: (Int, Boolean) => Boolean): Boolean
+
+}
+
+/**
+ * Empty list
+ */
+class Nil extends IntList {
+
+  override def toString = "nil"
+
+  def foreach(f: Int => Unit) {
+    ???
+  }
+
+  def map(f: Int => Int): IntList = ???
+
+  def filter(p: (Int) => Boolean) = ???
+
+  def sum = ???
+
+  def product = ???
+
+  def fold(z: Int, op: (Int, Int) => Int): Int = ???
+
+  def forall(p: Int => Boolean): Boolean = ???
+
+  def foldBool(z: Boolean, op: (Int, Boolean) => Boolean): Boolean = ???
+}
+
+/**
+ * List with a `head` element and a `tail` list
+ * @param head Head element
+ * @param tail Tail list
+ */
+class Cons(head: Int, tail: IntList) extends IntList {
+
+  override def toString = "cons(" + head + ", " + tail + ")"
+
+  def foreach(f: Int => Unit) {
+    ???
+  }
+
+  def map(f: Int => Int): IntList = ???
+
+  def filter(p: (Int) => Boolean) = ???
+
+  def sum = ???
+
+  def product = ???
+
+  def fold(z: Int, op: (Int, Int) => Int) = ???
+
+  def forall(p: (Int) => Boolean) = ???
+
+  def foldBool(z: Boolean, op: (Int, Boolean) => Boolean) = ???
+}
+
+/**
+ * Convenient factories
+ */
+object IntList {
+
+  /** @return an empty list */
+  def nil: IntList = new Nil
+
+  /** @return a list with a `head` element and a `tail` list */
+  def cons(head: Int, tail: IntList): IntList = new Cons(head, tail)
+
+  /** @example IntList(1, 2, 3) */
+  def apply(xs: Int*): IntList = xs.foldRight(nil)(cons)
+
+}
